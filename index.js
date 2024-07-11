2bfe953c2ecbdfc214ae6bb18efb9b01f1c83600
SShipon <nxshipon@gmail.com>
2024-07-11 14:24:45 +0100

    create a data put

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const corsConfig = {
 app.use(cors(corsConfig))
 app.options("", cors(corsConfig))
 
-
+// uri link 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u675lb8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
